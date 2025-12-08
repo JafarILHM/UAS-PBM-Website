4fe531523e11268a460d2945216631fff7618422
Akbar Putra Wiratama <148621758+AkbarPutraWiratama@users.noreply.github.com>
2025-12-09 00:05:07 +0700

    Replace entire project with new version

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,49 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('/charts-chartjs', function () {
+    return view('charts-chartjs');
+});
+
+Route::get('/icons-feather', function () {
+    return view('icons-feather');
+});
+
+Route::get('/maps-google', function () {
+    return view('maps-google');
+});
+
+Route::get('/pages-blank', function () {
+    return view('pages-blank');
+});
+
+Route::get('/pages-profile', function () {
+    return view('pages-profile');
+});
+
+Route::get('/pages-sign-in', function () {
+    return view('pages-sign-in');
+});
+
+Route::get('/pages-sign-up', function () {
+    return view('pages-sign-up');
+});
+
+Route::get('/ui-buttons', function () {
+    return view('ui-buttons');
+});
+
+Route::get('/ui-cards', function () {
+    return view('ui-cards');
+});
+
+Route::get('/ui-forms', function () {
+    return view('ui-forms');
+});
+
+Route::get('/ui-typography', function () {
+    return view('ui-typography');
 });
